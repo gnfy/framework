3766695b2250479b246771db5a7a4df95661de0d
gnfy <gnfy@163.com>
2013-05-23 14:11:11 +0800

    update index.php

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 /**
- * 入口文件
- *
- * License MIT GPL
- * Last modify: 2011-05-16 15:27:26
+ * ********************************************
+ * Description   : 入口文件
+ * Filename      : index.php
+ * Create time   : 2013-05-23 14:10:03
+ * Last modified : 2013-05-23 14:10:03
+ * License       : MIT, GPL
+ * ********************************************
  */
 include_once 'init/common.php';
 include_once 'init/router.php';      // 路由控制文件
