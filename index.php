2de1d765e5d06c3a47dbf289bb3293564c1ccbc9
root <root@andLinux>
2013-07-30 23:54:55 +0800

    更新index.php

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
  * Description   : 入口文件
  * Filename      : index.php
  * Create time   : 2013-05-23 14:10:03
- * Last modified : 2013-05-23 14:10:03
+ * Last modified : 2013-07-30 23:53:37
  * License       : MIT, GPL
  * ********************************************
  */
+
 include_once 'init/common.php';
 include_once 'init/router.php';      // 路由控制文件
